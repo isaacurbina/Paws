c301150fbaf0c788e8a1f387fa9def95d8b566c6
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-10 14:28:50 -0500

    first commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Isaac Urbina on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
