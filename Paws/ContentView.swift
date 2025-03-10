22eb52a09108772c03f97cd408703831c2c0046e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-03-10 15:10:32 -0500

    51. 1. SwiftData Project Setup

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -6,19 +6,32 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+	
+	@Environment(\.modelContext) var modelContext
+	@Query private var pets: [Pet]
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack {
+			ScrollView {
+				EmptyView()
+			}
+			.overlay {
+				if pets.isEmpty {
+					CustomContentUnavailableView(
+						icon: "dog.circle",
+						title: "No Pets",
+						description: "Add a new pet to get started."
+					)
+				}
+			}
+		}
     }
 }
 
-#Preview {
+#Preview("No data") {
     ContentView()
+		.modelContainer(for: Pet.self, inMemory: true)
 }
